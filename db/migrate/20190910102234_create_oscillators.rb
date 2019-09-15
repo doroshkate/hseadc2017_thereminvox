c2591772be5299312d3ae20869ad473b5e907706
Kate Dorosh <katyaflare@yandex.com>
2019-09-15 15:49:54 +0300

    Prettier Fixed

--- db/migrate/20190910102234_create_oscillators.rb
+++ db/migrate/20190910102234_create_oscillators.rb
@@ -2,10 +2,9 @@ class CreateOscillators < ActiveRecord::Migration[5.2]
   def change
     create_table :oscillators do |t|
       t.integer :frequency
-      t.string :wave, default: 'square' 
+      t.string :wave, default: 'square'
       t.integer :detune
 
       t.timestamps
     end
   end
-end
